7caa1bb17b66949b0504cda8c60c90c230b0af0e
Justin Hall <justin.r.hall@gmail.com>
2024-06-04 12:25:36 -0600

    Enable alwaysTryTypes in import/resolver settings

---
+++ .changeset/twelve-ads-count.md
@@ -0,0 +1,5 @@
+---
+'@wkovacs64/eslint-config': patch
+---
+
+Enable `alwaysTryTypes` in `import/resolver` settings.
--- index.js
+++ index.js
@@ -245,6 +245,13 @@ export const config = [
           '@typescript-eslint/require-await': OFF,
           '@typescript-eslint/unified-signatures': 'warn',
         },
+        settings: {
+          'import/resolver': {
+            typescript: {
+              alwaysTryTypes: true,
+            },
+          },
+        },
       }
     : null,
 
